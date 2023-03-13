8e1479a84fa4496fed18138959a2ee0482378209
Mammetnazar <nazar.rudn@gmail.com>
2023-03-13 12:08:26 +0300

    - more or less stable commit (no button multiple press bug, unlike yes button, where it disables the button) - ready for the mess with separating Alert

---
+++ MovieQuiz/Models/AlertModel.swift
@@ -0,0 +1,22 @@
+//
+//  AlertModel.swift
+//  MovieQuiz
+//
+//  Created by assistant on 13.03.2023.
+//
+
+import Foundation
+
+struct AlertModel {
+    let title:             String
+    let message:           String
+    let buttonText:        String
+    
+//    замыкание без параметров для действия по кнопке алерта completion
+    
+//    func completion(){
+//        self.title = nil
+//        self.message = nil
+//        self.buttonText = nil
+//    }
+}
---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by assistant on 13.03.2023.
+//
+
+import Foundation
