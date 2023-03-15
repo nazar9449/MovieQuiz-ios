baa2deae5095df95b7fe8debd2c484e6ee8b4fe7
Mammetnazar <nazar.rudn@gmail.com>
2023-03-15 23:07:48 +0300

    - private enum with coding keys added to MostPopularMovie class

--- MovieQuiz/Models/MostPopularMovies.swift
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -9,4 +9,10 @@ struct MostPopularMovie {
     let title: String
     let rating: String
     let imageURL: URL
+    
+    private enum CodingKeys: String, CodingKey {
+        case title = "fullTitle"
+        case rating = "imDbRating"
+        case imageURL = "image"
+    }
 }
